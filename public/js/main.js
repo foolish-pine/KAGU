16b70b68908866334f6aba91d542ab6ff0114d66
MatsudaTakuya <matsuda.takuya41@gmail.com>
2020-04-22 19:24:47 +0900

    fix: gulp導入/その他修正

---
+++ public/js/main.js
@@ -0,0 +1,138 @@
+/* ================================================================
+  jQuery
+   ================================================================ */
+
+$(function () {
+  // ---------------------------------------------
+  // ハンバーガーメニュー
+  // ---------------------------------------------
+
+  var mq = window.matchMedia("screen and (max-width:959px)");
+
+  var $headerNav = $(".p-header__nav"),
+    $hamburgerMenu = $(".js-hamburger-menu"),
+    $hamburgerMenuLine = $(".js-hamburger-menu-line"),
+    $footerSubNav = $(".p-footer__sub-nav"),
+    $crossMenu = $(".js-cross-menu"),
+    $crossMenuLine = $(".js-cross-menu-line");
+
+  $(window).on("resize", function () {
+    if (mq.matches) {
+      // 画面幅959px以下のとき
+      // navを非表示にする
+      $headerNav.hide();
+      $footerSubNav.hide();
+      // メニューアイコンを非activeにする
+      $hamburgerMenuLine.removeClass("active");
+      $crossMenuLine.removeClass("active");
+    } else {
+      // 画面幅960px以上のとき
+      // navを表示させる
+      $headerNav.show();
+      $footerSubNav.show();
+    }
+  });
+
+  // メニューアイコンをクリックしてnavを開閉する
+  $hamburgerMenu.on("click", function () {
+    $hamburgerMenuLine.stop(true).toggleClass("active");
+    $headerNav.stop(true).fadeToggle();
+  });
+
+  $crossMenu.on("click", function () {
+    $crossMenuLine.stop(true).toggleClass("active");
+    $footerSubNav.stop(true).fadeToggle();
+  });
+
+  // ナビの余白クリックでヘッダーメニュー閉じる
+  $headerNav.on("click", function () {
+    if ($hamburgerMenuLine.hasClass("active")) {
+      $hamburgerMenuLine.stop(true).toggleClass("active");
+      $headerNav.stop(true).fadeToggle();
+    }
+  });
+
+  // ---------------------------------------------
+  // スムーススクロール（ページ内リンク）
+  // ---------------------------------------------
+  $(".js-smoothscroll").click(function () {
+    var speed = 500,
+      href = $(this).attr("href"),
+      target = $(href == "#" || href == "" ? "html" : href),
+      headerHeight = $(".p-header").outerHeight(),
+      position = target.offset().top - headerHeight; // ヘッダーの高さ分スクロール量を減らす
+    $("html, body").animate({ scrollTop: position }, speed);
+  });
+
+  // ---------------------------------------------
+  // スムーススクロール（ページトップへ戻る）
+  // ---------------------------------------------
+  $(".js-pagetop").click(function () {
+    var speed = 500;
+    $("html, body").animate({ scrollTop: 0 }, speed);
+  });
+
+  // ---------------------------------------------
+  // スライドショー
+  // ---------------------------------------------
+  $(".p-main-visual__slideshow").each(function () {
+    var $container = $(this),
+      $slideGroup = $container.find(".p-main-visual__slideshow-slides"),
+      $slides = $slideGroup.find(".p-main-visual__slide"),
+      $indicator = $container.find(".p-main-visual__slideshow-indicator"),
+      slideCount = $slides.length,
+      indicatorHTML = "",
+      currentIndex = 0,
+      duration = 500,
+      interval = 7500,
+      timer;
+
+    $slides.each(function (i) {
+      $(this).css({ left: 100 * i + "%" });
+      indicatorHTML += '<a href="#"></a>';
+    });
+
+    $indicator.html(indicatorHTML);
+
+    function goToSlide(index) {
+      $slideGroup.animate({ left: -100 * index + "%" }, duration);
+      currentIndex = index;
+      updateNav();
+    }
+
+    function updateNav() {
+      $indicator
+        .find("a")
+        .removeClass("active")
+        .eq(currentIndex)
+        .addClass("active");
+    }
+
+    function startTimer() {
+      timer = setInterval(function () {
+        var nextIndex = (currentIndex + 1) % slideCount;
+        goToSlide(nextIndex);
+      }, interval);
+    }
+
+    function stopTimer() {
+      clearInterval(timer);
+    }
+
+    $indicator.on("click", "a", function (event) {
+      event.preventDefault();
+      if (!$(this).hasClass("active")) {
+        goToSlide($(this).index());
+      }
+    });
+
+    $container.on({
+      mouseenter: stopTimer,
+      mouseleave: startTimer,
+    });
+
+    goToSlide(currentIndex);
+
+    startTimer();
+  });
+});
